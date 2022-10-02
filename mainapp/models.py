4f10d229a7fb493752512c544c9a9644d2fbfb27
beforedisappear <roman23kl@gmail.com>
2022-10-03 02:11:56 +0300

    new css!!!

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,9 +1,8 @@
-from unittest.util import _MAX_LENGTH
 from django.db import models
 
 class articles(models.Model):
    title = models.CharField(max_length=100)
-   creator = models.CharField(max_lenght=20)
+   creator = models.CharField(max_length=20)
    subtitle = models.CharField(max_length=50)
    content = models.TextField()
    photo = models.ImageField()
